f56c8c295000ae8d129330e1010aed333c47ae50
Byongho96 <unlike96@gmail.com>
2022-09-18 15:36:34 +0900

    0918

--- 20924_트리의_기둥과_가지.py
+++ 20924_트리의_기둥과_가지.py
@@ -1,39 +1,42 @@
+# 912ms
 from collections import deque
 import sys
 input = sys.stdin.readline
 
-def bfs_root(v):
-    visited = [-1] * (N + 1)
-    q = deque()
-
-    visited[v[0]] = v[1]
-    q.append(v)
-
-    trunk = 0
-    giga = 0
-    while q:
-        v = q.popleft()
-        if not giga and len(adjLst[v[0]]) > 2:
-            trunk = visited[v[0]]
-            giga = 1
-        for w in adjLst[v[0]]:
-            if visited[w[0]] == -1:
-                q.append(w)
-                visited[w[0]] = visited[v[0]] + w[1]
-
-    if not giga:
-        trunk = max(visited)
-    return trunk, max(visited) - trunk
-
-
 N, R = map(int, input().split())
-
 adjLst = [[] for _ in range(N + 1)]
 for _ in range(N-1):
     a, b, d = map(int, input().split())
     adjLst[a].append((b, d))
     adjLst[b].append((a, d))
 
-trunk, branch = bfs_root((1, 0))
+def bfs(rt, dis):
+    distance = [-1] * (N + 1)
+    q = deque()
 
+    distance[rt] = dis
+    q.append(rt)
+
+    trunk, branch = 0, 0
+    flag = 0
+
+    if len(adjLst[rt]) > 1:
+        flag = 1
+
+    while q:
+        v = q.popleft()
+        if not flag and len(adjLst[v]) > 2:
+            trunk = distance[v]
+            flag = 1
+        for w in adjLst[v]:
+            if distance[w[0]] == -1:
+                q.append(w[0])
+                distance[w[0]] = distance[v] + w[1]
+
+    if not flag:
+        trunk = max(distance)
+    branch = max(distance) - trunk
+    return trunk, branch
+
+trunk, branch = bfs(R, 0)
 print(trunk, branch)
