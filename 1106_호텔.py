19bddfb089bf4deb40593386a5dd7665d674e578
Byongho <unlike96@gmail.com>
2022-10-18 17:58:44 +0900

    1015

--- 1106_호텔.py
+++ 1106_호텔.py
@@ -2,15 +2,81 @@ import sys
 input = sys.stdin.readline
 
 # 참조 블로그: https://hongcoding.tistory.com/m/50
-C, N = map(int, input().split())
 
-price = [0] * (N + 1)
-customer = [0] * (N + 1)
+# C, N = map(int, input().split())    # C: 목표인원, N: 도시갯수
+# price = [0] * (N + 1)
+# customer = [0] * (N + 1)
+# for i in range(1, N + 1):
+#     price[i], customer[i] = map(int, input().split())
+
+# # [1]. 2차원 DP [30840KB, 7000ms]
+# MAX = 100001
+# DP = [[0] + [MAX] * C for _ in range(N + 1)]    # DP 배열로 초깃값 주의
+# for cur_city in range(1, N + 1):
+#     for target_num in range(1, C + 1):
+#         m = 0
+#         while True:
+#             invited = customer[cur_city] * m
+#             cost = price[cur_city] * m
+#             if target_num >= invited:
+#                 DP[cur_city][target_num] = min(DP[cur_city][target_num], DP[cur_city - 1][target_num - invited] + cost)
+#             else:
+#                 DP[cur_city][target_num] = min(DP[cur_city][target_num], cost)
+#                 break
+#             m += 1
+#
+# print(DP[-1][-1])
+
+# # [2]. 1차원 DP [30840KB, 6004ms]
+# MAX = 100001
+# DP = [0] + [MAX] * C    # DP 배열로 초깃값 주의
+# for cur_city in range(1, N + 1):
+#     for target_num in range(C, 0, -1):
+#         m = 0
+#         while True:
+#             invited = customer[cur_city] * m
+#             cost = price[cur_city] * m
+#             if target_num >= invited:
+#                 DP[target_num] = min(DP[target_num], DP[target_num - invited] + cost)
+#             else:
+#                 DP[target_num] = min(DP[target_num], cost)
+#                 break
+#             m += 1
+#
+# print(DP[-1])
+
+#################################################################################################
+# [3]. 메모이제이션 [30840KB, 6132ms]
+def memoization(i, j):
+    if j <= 0:
+        return 0
+    if i == 0:
+        return MAX
+    if DP[i][j] != MAX:
+        return DP[i][j]
+    m = 0
+    while True:
+        cost = p_c[i][0] * m
+        invited = p_c[i][1] * m
+        if j >= invited:
+            DP[i][j] = min(DP[i][j], memoization(i-1, j - invited) + cost)
+        else:
+            DP[i][j] = min(DP[i][j], cost)
+            break
+        m += 1
+    if DP[i][j] == MAX:
+        DP[i][j] += 1
+    return DP[i][j]
+
+C, N = map(int, input().split())    # C: 목표인원, N: 도시갯수
+p_c = [[] for _ in range(N + 1)]
+p_c[0] = [0, 101]
 for i in range(1, N + 1):
-    price[i], customer[i] = map(int, input().split())
+    p_c[i] = list(map(int, input().split()))
 
-DP = [[0] * (C + 1) for _ in range(N+1)]
+p_c.sort(key=lambda x: x[1], reverse=True)  # 생각보다 별 효과 없음
+MAX = 100001
+DP = [[MAX] * (C + 1) for _ in range(N + 1)]
 
-for i in range(1, N + 1):
-    for j in range(1, C + 1):
-        if
+print(memoization(N, C))
+
--- 12865_평범한_배낭.py
+++ 12865_평범한_배낭.py
@@ -1,12 +1,53 @@
 import sys
 input = sys.stdin.readline
 
-N, K = int(input())
-weight = [0] * N
-value = [0] * N
-for i in range(N):
+N, K = map(int, input().split())
+weight = [0] * (N + 1)  # DP배열의 행과 인덱스를 맞추기 위해서
+value = [0] * (N + 1)   # DP배열의 행과 인덱스를 맞추기 위해서
+for i in range(1, N + 1):
     weight[i], value[i] = map(int, input().split())
 
-dp = [[0] * K for _ in range(N)]
-# dp[n][restweight] : 1번부터 n번
-dp[][]
+# # [1]. 2차원 DP [228MB, 6956ms]
+# DP = [[0] * (K + 1) for _ in range(N + 1)]
+# for cur_object in range(1, N + 1):
+#     for limit in range(1, K + 1):
+#         if limit >= weight[cur_object]:
+#             DP[cur_object][limit] = max(DP[cur_object-1][limit], DP[cur_object-1][limit-weight[cur_object]] + value[cur_object])
+#         else:
+#             DP[cur_object][limit] = DP[cur_object - 1][limit]
+# print(DP[-1][-1])
+
+# # [2]. 1차원 DP [34MB, 5140ms]
+# DP = [0] * (K + 1)
+# for cur_object in range(1, N + 1):
+#     for limit in range(K, 0, -1):   # 그래야지 DP[cur_object-1][]값을 유효하게 참조할 수 있다.
+#         if limit >= weight[cur_object]:
+#             DP[limit] = max(DP[limit], DP[limit-weight[cur_object]] + value[cur_object])
+# print(DP[-1])
+
+# # [3]. 메모이제이션 [139MB, 3048ms]
+# # memoization 할 필요가 없음. 상태공간이 겹칠 확률이 매우 적기는 하다.
+# def memoization(i, j):
+#     if i <= 0 or j <= 0:
+#         return 0
+#     if DP[i][j]:
+#         return DP[i][j]
+#     if j >= weight[i]:
+#         DP[i][j] = max(memoization(i-1, j), memoization(i-1, j-weight[i]) + value[i])
+#     else:
+#         DP[i][j] = memoization(i-1, j)
+#     return DP[i][j]
+#
+# DP = [[0] * (K + 1) for _ in range(N + 1)]
+# print(memoization(N, K))
+
+# [4]. 메모이제이션 없는 재귀호출 [시간초과]
+def recursive(i, j):
+    if i <= 0 or j <= 0:
+        return 0
+    if j >= weight[i]:
+        return max(recursive(i-1, j), recursive(i-1, j-weight[i]) + value[i])
+    else:
+        return recursive(i-1, j)
+
+print(recursive(N, K))
