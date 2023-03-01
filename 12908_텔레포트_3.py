32a3f2fffbb15cbc1e3be52163c486a21c95bbc0
Byongho96 <unlike96@gmail.com>
2023-03-01 21:22:52 +0900

    2263_트리의_순회

--- 12908_텔레포트_3.py
+++ 12908_텔레포트_3.py
@@ -1,58 +1,123 @@
-from collections import defaultdict
 import heapq
 
-def bfs_backtracking(x, y):
-    global mn
+nodes = []  # 8개 노드 저장할 리스트
+adjLst ={}  # 노드마다 인접 노드리스트를 저장할 객체 -> 다익스트라
 
-    visited = defaultdict(int)
-    q = [(1, x, y)]
-    visited[(x, y)] = 1
-
-    while q:
-        n, x, y = heapq.heappop(q)
-        # 종료조건
-        if x == xe and y == ye:
-            mn = min(mn, n)
-            continue
-        # 가지치기
-        if n >= mn:
-            break
-        # 후보군 출력
-        for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx <= 1000000000 and 0 <= ny <= 1000000000 and (not visited[(nx, ny)] or visited[(nx, ny)] > n + 1):
-                heapq.heappush(q, (n+1, nx, ny))
-                visited[(nx, ny)] = n + 1
-
-        if (x, y) in teleport1:
-            index = teleport1.index((x ,y))
-            nx, ny = teleport2[index]
-            if (not visited[(nx, ny)] or visited[(nx, ny)] > n + 10):
-                heapq.heappush(q, (n+10, nx, ny))
-                visited[(nx, ny)] = n + 10
-
-        elif (x, y) in teleport2:
-            index = teleport2.index((x ,y))
-            nx, ny = teleport1[index]
-            if (not visited[(nx, ny)] or visited[(nx, ny)] > n + 10):
-                heapq.heappush(q, (n+10, nx, ny))
-                visited[(nx, ny)] = n + 10
-
+# 단순 입력
 xs, ys = map(int, input().split())
 xe, ye = map(int, input().split())
+nodes.append((xs, ys))
+nodes.append((xe, ye))
+adjLst[(xs, ys)] = []
+adjLst[(xe, ye)] = []
 
 teleport1 = []
 teleport2 = []
-
 for _ in range(3):
     x1, y1, x2, y2 = map(int, input().split())
     teleport1.append((x1, y1))
     teleport2.append((x2, y2))
+    nodes.extend([(x1, y1), (x2, y2)])
+    adjLst[(x1, y1)] = []
+    adjLst[(x2, y2)] = []
 
-mn = 2000000000
-bfs_backtracking(xs, ys)
-print(mn-1)
+# adjLst 생성
+for i in range(8):
+    node1 = nodes[i]
+    x1, y1 = node1
+    for j in range(i):
+        node2 = nodes[j]
+        x2, y2 = node2
+        adjLst[node2].append((abs(x1-x2) + abs(y1-y2), x1, y1))
+        adjLst[node1].append((abs(x1-x2) + abs(y1-y2), x2, y2))
+
+for i in range(3):
+    node1 = teleport1[i]
+    x1, y1 = node1
+    node2 = teleport2[i]
+    x2, y2 = node2
+    adjLst[node1].append((10, x2, y2))
+    adjLst[node2].append((10, x1, y1))
+
+# heapq를 이용한 다익스트라
+def dijkstra(xs, ys):
+    distance = {}
+    for node in nodes:
+        distance[node] = 2000000000
+    
+    heap = []
+    heapq.heappush(heap, (0, xs, ys))
+    distance[(xs, ys)] = 0
+
+    while heap:
+        dist, x, y = heapq.heappop(heap)
+        if distance[(x, y)] < dist:
+            continue
+        for w, nx, ny in adjLst[(x, y)]:
+            new_dist = dist + w
+            if new_dist < distance[(nx, ny)]:
+                distance[(nx, ny)] = new_dist
+                heapq.heappush(heap, (new_dist, nx, ny))
+
+    return distance[(xe, ye)]
+
+print(dijkstra(xs, ys))
+
+# from collections import defaultdict
+# import heapq
+
+# def bfs_backtracking(x, y):
+#     global mn
+
+#     visited = defaultdict(int)
+#     q = [(1, x, y)]
+#     visited[(x, y)] = 1
+
+#     while q:
+#         n, x, y = heapq.heappop(q)
+#         # 종료조건
+#         if x == xe and y == ye:
+#             mn = min(mn, n)
+#             continue
+#         # 가지치기
+#         if n >= mn:
+#             break
+#         # 후보군 출력
+#         for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+#             nx = x + dx
+#             ny = y + dy
+#             if 0 <= nx <= 1000000000 and 0 <= ny <= 1000000000 and (not visited[(nx, ny)] or visited[(nx, ny)] > n + 1):
+#                 heapq.heappush(q, (n+1, nx, ny))
+#                 visited[(nx, ny)] = n + 1
+
+#         if (x, y) in teleport1:
+#             index = teleport1.index((x ,y))
+#             nx, ny = teleport2[index]
+#             if (not visited[(nx, ny)] or visited[(nx, ny)] > n + 10):
+#                 heapq.heappush(q, (n+10, nx, ny))
+#                 visited[(nx, ny)] = n + 10
+
+#         elif (x, y) in teleport2:
+#             index = teleport2.index((x ,y))
+#             nx, ny = teleport1[index]
+#             if (not visited[(nx, ny)] or visited[(nx, ny)] > n + 10):
+#                 heapq.heappush(q, (n+10, nx, ny))
+#                 visited[(nx, ny)] = n + 10
+
+# xs, ys = map(int, input().split())
+# xe, ye = map(int, input().split())
+
+# teleport1 = []
+# teleport2 = []
+
+# for _ in range(3):
+#     x1, y1, x2, y2 = map(int, input().split())
+#     teleport1.append((x1, y1))
+#     teleport2.append((x2, y2))
+
+# mn = 2000000000
+# bfs_backtracking(xs, ys)
+# print(mn-1)
 
 
 
--- 2263_트리의_순회.py
+++ 2263_트리의_순회.py
@@ -1,36 +1,78 @@
-# https://ku-hug.tistory.com/135?category=978336
-
+# 참고 글: https://velog.io/@dark6ro/%EB%B0%B1%EC%A4%80-2263%EB%B2%88-%ED%8A%B8%EB%A6%AC%EC%9D%98-%EC%88%9C%ED%9A%8C
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6)
 
-# 304 ms
-# postorder와 inorder모두 처음인덱스와 끝인덱스로 넘김
-# search하지 않고 index로 찾을 수 있는 리스트 새로 생성
-N = int(input())
-inorder = list(map(int, input().split()))
-postorder = list(map(int, input().split()))
+in_order = []
+post_order = []
+pre_order = []
 
-def make_tree(in_start, in_end, post_start, post_end):
-    global post_ptr
-    if in_end < in_start or post_end < post_start:
+def make_pre_order(N, in_start, in_end, post_start, post_end):
+    # 분할정복의 종료조건
+    if N < 1:
         return
-    root = postorder[post_end]
-    idx = inorder_idx[root]
-    size = idx - in_start
-    # print('root', root, 'idx', idx, 'size', size)
+    
+    # 포스트 오더의 마지막 변수 -> 인 오더의 루트 값
+    root = post_order[post_end]
+    pre_order.append(root)
 
-    print(root, end=' ')
+    root_idx = in_order_num_to_idx[root]
+    left_size = root_idx - in_start
+    right_size = in_end - root_idx
 
-    make_tree(in_start, idx-1, post_start, post_start + size -1)
-    make_tree(idx + 1, in_end, post_start + size, post_end -1)
+    # 사이즈를 기반으로 in_order와 post_order 시작/끝 인덱스 계산
+    make_pre_order(left_size, in_start, root_idx-1, post_start, post_start+left_size-1)
+    make_pre_order(right_size, root_idx+1, in_end, post_end-right_size, post_end-1)
+    return
 
-# idx를 구하는 리스트를 미리 형성
-inorder_idx = [0] * (N + 1)
-for i in range(N):
-    inorder_idx[inorder[i]] = i
+if __name__ == "__main__":
+    N = int(input())
+    in_order = list(map(int, input().split()))
+    post_order = list(map(int, input().split()))
 
-make_tree(0, N-1, 0, N-1)
+    # 인오더에서 num -> idx를 찾아주는 배열 (없으면 시간초과)
+    in_order_num_to_idx = [0] * (N + 1)
+    for i in range(N):
+        in_order_num_to_idx[in_order[i]] = i
+
+    make_pre_order(N, 0 , N-1, 0, N-1)
+    print(*pre_order)
+
+
+
+#####################################################
+# # https://ku-hug.tistory.com/135?category=978336
+
+# import sys
+# input = sys.stdin.readline
+# sys.setrecursionlimit(10**9)
+
+# # 304 ms
+# # postorder와 inorder모두 처음인덱스와 끝인덱스로 넘김
+# # search하지 않고 index로 찾을 수 있는 리스트 새로 생성
+# N = int(input())
+# inorder = list(map(int, input().split()))
+# postorder = list(map(int, input().split()))
+
+# def make_tree(in_start, in_end, post_start, post_end):
+#     global post_ptr
+#     if in_end < in_start or post_end < post_start:
+#         return
+#     root = postorder[post_end]
+#     idx = inorder_idx[root]
+#     size = idx - in_start
+#     # print('root', root, 'idx', idx, 'size', size)
+
+#     print(root, end=' ')
+
+#     make_tree(in_start, idx-1, post_start, post_start + size -1)
+#     make_tree(idx + 1, in_end, post_start + size, post_end -1)
+
+# # idx를 구하는 리스트를 미리 형성
+# inorder_idx = [0] * (N + 1)
+# for i in range(N):
+#     inorder_idx[inorder[i]] = i
+
+# make_tree(0, N-1, 0, N-1)
 
 #####################################################
 # 시간초과
