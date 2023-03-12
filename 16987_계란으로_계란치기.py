d95fb60ccadd82dfebc88be57fc412cf5ebdb975
Byongho96 <unlike96@gmail.com>
2023-03-12 19:33:46 +0900

    16987_계란으로 바위치기

--- 16987_계란으로_계란치기.py
+++ 16987_계란으로_계란치기.py
@@ -7,19 +7,21 @@ def backtracking(cur, N):
         return
 
     # 2. 가지 치기
-    if sum(broken) == N:
+    if max_broken == N:     # 이미 다른 경우에서 최대 값이 나온 경우 -> 종료
+        return
+    
+    if sum(broken) == N:    # 최대 값을 도달한 경우 -> max_broken 업데이트 후 종료
         max_broken = N
         return
     
-    if max_broken == N:
-        return
-    
     # 3. 후보군 출력
-    if broken[cur] or sum(broken) == N-1:
+    if broken[cur] or sum(broken) == N-1:   # 손에 든 달걀이 깨졌거나, 다른 달걀들이 모두 깨졌으면 -> 다음으로 넘어감
         backtracking(cur + 1, N)
         return
 
-    for tar in range(N):
+    for tar in range(N):    # 깨지지 않은 다른 달걀이 있으면 깨기
+        if broken[tar]:
+            continue
         if tar == cur:
             continue
         # 계란 깨기
@@ -30,7 +32,7 @@ def backtracking(cur, N):
         if durability[cur] <= 0:
             broken[cur] = 1
         backtracking(cur + 1, N)
-        # 계란 복구
+        # 다음 반복문을 위해 계란 복구
         durability[tar] += weight[cur]
         durability[cur] += weight[tar]
         if durability[tar] > 0:
@@ -39,8 +41,6 @@ def backtracking(cur, N):
             broken[cur] = 0
 
     return 
-
-
 
 N = int(input())
 
