057e6f30a5d58c2d60ad511721f0dd7ffa94346a
Byongho96 <unlike96@gmail.com>
2023-09-10 14:32:08 +0900

    algrothim practice

--- Programmers/2022_KAKAO_TECH_INTERSHIP/두_튜_합_같게_만들기.py
+++ Programmers/2022_KAKAO_TECH_INTERSHIP/두_튜_합_같게_만들기.py
@@ -1,8 +1,32 @@
-from collections import deque
-
 def solution(queue1, queue2):
-    q1 = deque(queue1)
-    q2 = deque(queue2)
+    # remember the queue1 length
+    Q1 = len(queue1)
+    sm = sum(queue1)
 
-    answer = -2
-    return answer
+    # merge the queues
+    queue1.extend(queue2)
+    Q = len(queue1)
+    SUM = sum(queue1)
+
+    # filter the invalid case
+    if SUM % 2:
+        return -1
+    SUM //= 2
+
+    # two pointer
+    # WARNING: The end condition is not the one iteration
+    start, end = 0, Q1 - 1
+    answer = 0
+    for _ in range(2 * Q):
+        if sm < SUM:
+            end = (end + 1) % Q
+            answer += 1
+            sm += queue1[end]
+        elif sm > SUM:
+            sm -= queue1[start]
+            start = (start + 1) % Q
+            answer += 1
+        else:
+            return answer
+
+    return -1
