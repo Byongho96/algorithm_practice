716e4d096b8b6bee6ba9e16bd141f5a36492df3f
Byongho <unlike96@gmail.coom>
2024-05-30 09:37:38 +0900

    algorithm practice

--- Baekjoon/17836_공주님을_구해라.py
+++ Baekjoon/17836_공주님을_구해라.py
@@ -1,92 +1,55 @@
-from collections import deque
+import sys
+import heapq
 
-def bfs(N, M, T):
-    q = deque()
-    visited = [[0] * M for _ in range(N)]
+input = sys.stdin.readline
 
-    q.append((0, 0))
-    visited[0][0] = 1
+INF = 100 * 100 + 1
+DIRECTION = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
-    sword_time = N * M
-    while q:
-        i, j= q.popleft()
-        t = visited[i][j]
-        # 종료 조건
-        if i == N - 1 and j == M - 1:
-            return min(t - 1, sword_time)
-        # 가지 치기
+def a_star(N, M, T, arr):
+    distance = [[[INF, INF] for _ in range(M)] for _ in range(N)]
+
+    distance[0][0][0] = 0
+    heap = [(N - 1 + M - 1, 0, 0, 0, 0)] # estimated, time, i, j, isGram
+
+    while heap:
+        h, t, i, j, isGram = heapq.heappop(heap)
+
+        # pruning
+        if t > distance[i][j][isGram]:
+            continue
+
+        # early end condition
         if t > T:
+            return False
+
+        # end condition
+        if i == N - 1 and j == M - 1:
             return t
-        if t > sword_time:
-            return sword_time
-        # bfs
-        for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+        
+        # visit the node
+        if arr[i][j] == 2:
+            isGram = True
+
+        # traverse adjacent nodes
+        for di, dj in DIRECTION:
             ni = i + di
             nj = j + dj
-            if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj]:
-                visited[ni][nj] = t + 1
-                if arr[ni][nj] == 0:
-                    q.append((ni, nj))
-                    continue
-                if arr[ni][nj] == 2:
-                    sword_time = t + (N - 1 - ni) + (M - 1 - nj)
-                    continue    
+            nt = t + 1
+            nh = nt + (N - 1 - ni) + (M - 1 - nj)
+            if -1 < ni < N and -1 < nj < M and (arr[ni][nj] != 1 or isGram) and distance[ni][nj][isGram] > nt:
+                distance[ni][nj][isGram] = nt
+                heapq.heappush(heap, (nh, nt, ni, nj, isGram))
 
-    return sword_time # 정석적인 방법으로 도착하지 못한 경우
+    return False
 
 if __name__ == '__main__':
-    # 입력
     N, M, T = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    arr =  [list(map(int, input().split())) for _ in range(N)]
 
-    # bfs 결과
-    answer = bfs(N, M, T)
+    answer = a_star(N, M, T, arr)
 
-    if answer > T or answer >= N * M:
-        print('Fail')
-    else:
-        print(answer)
+    print (answer if answer else 'Fail')
 
 
-
-# from collections import deque
-# import sys
-# input = sys.stdin.readline
-
-# # 96ms
-# def bfs(i, j):
-#     visited = [[0] * M for _ in range(N)]
-#     q = deque()
-
-#     visited[i][j] = 1
-#     q.append((i, j))
-
-#     sword = N * M
-#     princess = N * M
-#     while q:
-#         i, j = q.popleft()
-#         if arr[i][j] == 2:
-#             sword = visited[i][j]-1 + N-1-i + M-1-j
-#         if i == N - 1 and j == M - 1:
-#             princess = visited[i][j] - 1
-#         for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-#             ni = i + di
-#             nj = j + dj
-#             if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj] and arr[ni][nj] != 1:
-#                 visited[ni][nj] = visited[i][j] + 1
-#                 q.append((ni, nj))
-#     # 검과 공주 둘다한테 못갈 수도 있음!!
-#     return min(sword, princess)
-
-
-# N, M, T = map(int, input().split())
-# arr = [list(map(int, input().split())) for _ in range(N)]
-
-# result = bfs(0, 0)
-# if result == M*N:
-#     print('Fail')
-# elif result <= T:
-#     print(result)
-# else:
-#     print('Fail')
 
