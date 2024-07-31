01aa5d77efa6691e5485bbc6261f4668d61db052
Byongho <unlike96@gmail.coom>
2024-07-31 09:19:44 +0900

    algorithm practice

--- Baekjoon/23040_누텔라_트리_Easy.py
+++ Baekjoon/23040_누텔라_트리_Easy.py
@@ -1,46 +1,116 @@
-
 from typing import List, Tuple
+from collections import defaultdict
 import sys
 
 input = sys.stdin.readline
 
-def solution(N:int, adjLst:Tuple[List[int]], clrs:str) -> int:
-    memo = {}
-    answer = 0
-    colors = 'G' + clrs
+# DFS
+def memoize(N: int, adjLst: Tuple[List[int]], colors: str, start: int) -> List[int]:
+    memo = [-1] * (N + 1) 
+    red_group = defaultdict(set)
+    stack = []
 
-    for start in range(1, len(colors)):
-        color = colors[start]
-        if color != 'B':
-            continue
+    cur, black = start, start
 
-        visited = [False] * (N + 1)
-        stack = []
-        cur = start
+    while True:
+        # 노드 방문
+        memo[cur] = 0
 
-        while True:
-            for adj in adjLst[cur]:
-                if visited[adj] or colors[adj] != 'R' or (cur, adj) in memo:
-                    continue
-                stack.append(cur)
-                cur = adj
-                visited[cur] = True
-                break
+        # 깊이 우선 탐색
+        for adj in adjLst[cur]:
+
+            if memo[adj] > -1:
+                continue
+
+            stack.append((cur, black))
+
+            cur = adj
+            if colors[cur] == 'B':
+                black = cur
+            else:
+                red_group[black].add(cur)
+            break
+
+        else:
+            if stack:
+                cur, black = stack.pop()
+                if cur == black: # colors[cur] == 'B'
+                    num_red = len(red_group[cur])
+                    for red in red_group[cur]:
+                        memo[red] = num_red
+                    del red_group[cur]
 
             else:
-                if stack:
-                    sm = 1
-                    for adj in adjLst[cur]:
-                        sm += memo.get((cur, adj), 0)
-                    memo[(stack[-1], cur)] = sm
-                    cur = stack.pop()
-                else:
-                    for adj in adjLst[cur]:
-                        answer += memo.get((cur, adj), 0)
-                    break
+                break
+
+    return memo
+
+
+def solution(N:int, adjLst:Tuple[List[int]], colors:str) -> int:
+    black = colors.find('B')
+    if black < 0:
+        return  0
+    
+    memo = memoize(N, adjLst, colors, black)
+
+    answer = 0
+    for start in range(1, len(colors)):
+        if colors[start] != 'B':
+            continue
+
+        for adj in adjLst[start]:
+            answer += memo[adj]
 
     return answer
 
+def find(parent, u):
+    while parent[u] != u:
+        u = parent[u]
+    return u
+
+def union_by_rank(parent, rank, size, u, v):
+    u = find(parent, u)
+    v = find(parent, v)
+
+    if u == v:
+        return
+
+    if rank[u] > rank[v]:
+        u, v = v, u
+
+    parent[u] = v
+    size[v] += size[u]
+    if rank[u] == rank[v]:
+        rank[v] += 1
+
+def solution2(N:int, adjLst:Tuple[List[int]], colors:str) -> int:
+    # divide red and black
+    red_set = set()
+    black_set = set()
+    for i in range(1, N + 1):
+        if colors[i] == 'R':
+            red_set.add(i)
+        else:
+            black_set.add(i)
+
+    # connect red
+    parent = [i for i in range(N + 1)]
+    size = [1] * (N + 1)
+    rank = [0] * (N + 1)
+
+    for red in red_set:
+        for adj in adjLst[red]:
+            if adj in red_set:
+                union_by_rank(parent, rank, size, red, adj)
+
+    # calculate answer
+    answer = 0  
+    for black in black_set:
+        for adj in adjLst[black]:
+            if adj in red_set:
+                answer += size[find(parent, adj)]
+    
+    return answer
 
 if __name__ == "__main__":
     N = int(input())
@@ -51,7 +121,7 @@ if __name__ == "__main__":
         adjLst[u].append(v)
         adjLst[v].append(u)
 
-    colors = input().rstrip()
+    colors = 'G' + input().rstrip()
 
-    answer = solution(N, adjLst, colors)
+    answer = solution2(N, adjLst, colors)
     print(answer)
